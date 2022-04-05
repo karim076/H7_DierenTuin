3155b0643a991b26838ffeb9527d982bbc2bbb0c
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-05 10:16:48 +0200

    Added res variable and resolution updates

--- Dierentuin/MainPage.xaml.cs
+++ Dierentuin/MainPage.xaml.cs
@@ -8,6 +8,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.Storage.Pickers;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -23,6 +24,20 @@ namespace Dierentuin
         public MainPage()
         {
             this.InitializeComponent();
+            var bounds = ApplicationView.GetForCurrentView().VisibleBounds;
+            var reso = Convert.ToString(bounds);
+            res.Text = reso;
+            this.SizeChanged += MainPage_SizeChanged;
+        }
+
+        private void MainPage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var bounds = ApplicationView.GetForCurrentView().VisibleBounds;
+            var reso = bounds.Width;
+            if (reso < 600)
+            {
+                //resmin1.Fontsize = 10;
+            }
         }
 
         private async void Button_Click(object sender, RoutedEventArgs e)
